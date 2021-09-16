a16f367bd8b92fef3f8574fe3ed288d2f55e1cb7
SVITAROP <90199717+SVITAROP@users.noreply.github.com>
2021-09-16 14:42:57 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -56,7 +56,7 @@ Type https://t.me/moviemadh
         sendMarkup(start_string, context.bot, update, reply_markup)
     else:
         sendMarkup(
-            'Oops! not a Authorized user.\nPlease Request to @MrrAbhi He is GOD <b>slam-mirrorbot</b>.',
+            'Oops! not a Authorized user.\nPlease Request to @MrrAbhi He is GOD <b>@moviemadh mirrorbot</b>.',
             context.bot,
             update,
             reply_markup,
